9ab885e682dc8a8da1f9485c22637fb5da46d723
mdemers <mdemers@hagley.org>
2018-06-28 14:21:13 +0000

    changing error language

--- js/player.js
+++ js/player.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
   videojs("islandora_videojs").setup({
     file: Drupal.settings.islandora_videojs.url,
-    image: Drupal.settings.islandora_videojs.thumbnail
+    image: Drupal.settings.islandora_videojs.thumbnail,
+    preload: auto
   });
 });
